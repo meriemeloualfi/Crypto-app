deb877b3cd5112066dde2dbbcbbdaa9540f8a13b
meriemeloualfi <meriem.eloualfi@gmail.com>
2024-09-02 10:56:27 +0100

    update

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
-import DOMPurify from 'dompurify'
+ 
 
 import './Coin.css'
 
